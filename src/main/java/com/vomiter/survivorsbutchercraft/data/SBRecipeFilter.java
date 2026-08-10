8c77f29e80d2571b371b303cd42d5b12c6213cfd
vomiter-scp-zh <97169118+vomiter-scp-zh@users.noreply.github.com>
2026-08-11 02:19:18 +0800

    1. add cache system for rendering 2. fix meat hook loot table 3. fix some recipes using invalid ingredient 4. fix butcher block being unable to craft

--- src/main/java/com/vomiter/survivorsbutchercraft/data/SBRecipeFilter.java
+++ src/main/java/com/vomiter/survivorsbutchercraft/data/SBRecipeFilter.java
@@ -3,6 +3,7 @@ package com.vomiter.survivorsbutchercraft.data;
 import com.lance5057.butchercraft.Butchercraft;
 import com.lance5057.butchercraft.ButchercraftBlocks;
 import com.lance5057.butchercraft.ButchercraftRecipes;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.crafting.Recipe;
@@ -30,7 +31,7 @@ public class SBRecipeFilter {
 
     }
 
-    public static boolean shouldBlock(ResourceLocation id, Recipe<?> recipe){
+    public static boolean shouldBlock(ResourceLocation id, Recipe<?> recipe, RegistryAccess access){
         if(recipe.getType() == (ButchercraftRecipes.GRINDER.get())) return false;
         if (id.getPath().equals("butcherblock")) return false;
         if(id.getNamespace().equals(Butchercraft.MOD_ID)) return true;
